4aeaa4e14031e4690bda825ba990a958dbe31925
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-09-21 09:25:05 +0500

    Update MeteorPlusAddon.java

--- src/main/java/nekiplay/meteorplus/MeteorPlusAddon.java
+++ src/main/java/nekiplay/meteorplus/MeteorPlusAddon.java
@@ -258,7 +258,7 @@ public class MeteorPlusAddon extends MeteorAddon {
 
 	@Override
 	public String getWebsite() {
-		return "https://meteor-plus.com/";
+		return "https://meteorclientplus.github.io";
 	}
 
 	@Override
